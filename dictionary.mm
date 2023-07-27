eedd609dbb98f3cc7d5a52f7b24d3fa7ef6a9ba3
John Lindquist <johnlindquist@gmail.com>
2023-07-27 16:46:09 -0600

    fix(js): use js objects instead of json

--- dictionary.mm
+++ dictionary.mm
@@ -1,91 +1,72 @@
 // dictionary.mm
 
-// Include headers required for the module
 #include <napi.h>
 #include <Foundation/Foundation.h>
 #include <AppKit/AppKit.h>
 
-// N-API namespace usage for convenience
 using namespace Napi;
 
-// The method performing the main logic of the module
 Value Method(const CallbackInfo& info) {
-    // Get the execution environment
     Env env = info.Env();
 
-    // Check for correct number of arguments
     if (info.Length() < 1) {
-        // Throw a JavaScript TypeError
         TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
         return env.Null();
     }
 
-    // Check for correct argument type
     if (!info[0].IsString()) {
-        // Throw a JavaScript TypeError
         TypeError::New(env, "Argument must be a string").ThrowAsJavaScriptException();
         return env.Null();
     }
 
-    // Extract the string from the arguments
     String utf8_value = info[0].As<String>();
-    // Convert it to NSString for further processing
     NSString *inputWord = [NSString stringWithUTF8String:utf8_value.Utf8Value().c_str()];
-
-    // Create an instance of NSSpellChecker
     NSSpellChecker *spellChecker = [NSSpellChecker sharedSpellChecker];
 
-    // Get suggestions for the word
-    NSArray *suggestions = [spellChecker guessesForWordRange:NSMakeRange(0, [inputWord length]) 
-                                                   inString:inputWord 
-                                                   language:@"en_US" 
-                                                     inSpellDocumentWithTag:0];
-    
-    // Initialize the result array
+    // Initialize a JavaScript array
     Array results = Array::New(env);
     int i = 0;
-    
-    // Iterate over the suggestions
-    for (NSString *suggestedWord in suggestions) {
-        // Get the definition of the suggested word
-        NSString *definition = (__bridge_transfer NSString*)DCSCopyTextDefinition(NULL, (__bridge CFStringRef)suggestedWord, CFRangeMake(0, [suggestedWord length]));
-        
-        // Create a JavaScript object for the entry
-        Object entry = Object::New(env);
-        entry.Set("suggestion", String::New(env, [suggestedWord UTF8String]));
-        entry.Set("definition", String::New(env, [definition UTF8String]));
 
-        // Add the entry to the results array
+    // Fetch the definition for the original word and insert it into the results array
+    NSString *definition = (__bridge_transfer NSString*)DCSCopyTextDefinition(NULL, (__bridge CFStringRef)inputWord, CFRangeMake(0, [inputWord length]));
+
+    if (definition) {
+        Object entry = Object::New(env);
+        entry.Set(String::New(env, "suggestion"), String::New(env, [inputWord UTF8String]));
+        entry.Set(String::New(env, "definition"), String::New(env, [definition UTF8String]));
+
         results[i] = entry;
         i++;
     }
 
-    // Check if the original word is spelled correctly
-    if ([spellChecker checkSpellingOfString:inputWord startingAt:0].length == [inputWord length]) {
-        // Get the definition of the word
-        NSString *definition = (__bridge_transfer NSString*)DCSCopyTextDefinition(NULL, (__bridge CFStringRef)inputWord, CFRangeMake(0, [inputWord length]));
+    NSArray *suggestions = [spellChecker guessesForWordRange:NSMakeRange(0, [inputWord length]) 
+                                                   inString:inputWord 
+                                                   language:@"en_US" 
+                                                     inSpellDocumentWithTag:0];
 
-        // Create a JavaScript object for the entry
+    for (NSString *suggestedWord in suggestions) {
+        NSString *suggestedDefinition = (__bridge_transfer NSString*)DCSCopyTextDefinition(NULL, (__bridge CFStringRef)suggestedWord, CFRangeMake(0, [suggestedWord length]));
+
+        // Check if definition exists, if not, set to a default value
+        if (!suggestedDefinition) {
+            suggestedDefinition = @"Definition not found.";
+        }
+
+        // Create a JavaScript object directly
         Object entry = Object::New(env);
-        entry.Set("suggestion", String::New(env, [inputWord UTF8String]));
-        entry.Set("definition", String::New(env, [definition UTF8String]));
+        entry.Set(String::New(env, "suggestion"), String::New(env, [suggestedWord UTF8String]));
+        entry.Set(String::New(env, "definition"), String::New(env, [suggestedDefinition UTF8String]));
 
-        // Add the entry to the results array
         results[i] = entry;
+        i++;
     }
 
-    // Return the results array
-    return results;
+    return results; // Return the JavaScript array directly
 }
 
-// Function for initializing the module
 Object Init(Env env, Object exports) {
-    // Add the method to the exports object
     exports.Set(String::New(env, "lookup"), Function::New(env, Method));
-
-    // Return the modified exports object
     return exports;
 }
 
-// Use the NODE_API_MODULE macro to register the module
 NODE_API_MODULE(addon, Init)
